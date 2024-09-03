0cd56de7f785dc6af9f7c757decc8870058d33a9
JonaSeguReymundo <00043823@uca.edu.sv>
2024-09-03 14:53:13 -0600

    Terminando comentarios del ejercicio 04

--- Recursos-Labo-03/ejercicio-04/assets/js/main.js
+++ Recursos-Labo-03/ejercicio-04/assets/js/main.js
@@ -1,6 +1,6 @@
 const createCounter = () => {
     let counter = 0;
-    // La función que incrementa el número y retorna
+    // Retorna la función que incrementa el número y retorna.
     return () => {
         counter++;
         return counter;
@@ -15,8 +15,8 @@ const requestNumber = () => {
     let number;
     do {
         number = prompt("Introduce un número entero hasta el cual desee contar:");
-        number = parseInt(number); // Se convierte el número a entero
-    } while (isNaN(number) || number < 0);
+        number = parseInt(number); // Se convierte el número a entero.
+    } while (isNaN(number) || number < 0); // La condición del Loop para que no sea nulo o menor a cero.
 
     return number;
 }
