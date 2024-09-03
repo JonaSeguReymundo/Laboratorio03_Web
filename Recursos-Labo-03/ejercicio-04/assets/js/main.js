a7aa339a7ecd9449f0db86f5c95b6421f7d7aa0f
JonaSeguReymundo <00043823@uca.edu.sv>
2024-09-03 14:46:24 -0600

    Ejercicio 04

--- Recursos-Labo-03/ejercicio-04/assets/js/main.js
+++ Recursos-Labo-03/ejercicio-04/assets/js/main.js
@@ -1,17 +1,35 @@
 const createCounter = () => {
-    //Code
+    let counter = 0;
+    return () => {
+        counter++;
+        return counter;
+    }
 }
 
-const showCounter = (/*recibe*/) => {
-    //Code
+const showCounter = (counter) => {
+    alert(counter);
 }
 
-const requestNumber=()=>{
-    
+const requestNumber = () => {
+    let number;
+    do {
+        number = prompt("Introduce un número entero hasta el cual desee contar:");
+        number = parseInt(number, 10);
+    } while (isNaN(number) || number < 0);
+
+    return number;
 }
 
 const main = () => {
-   
+    const number = requestNumber();
+
+    const counter = createCounter();
+
+    for (let i = 0; i < number; i++) {
+        const currentCount = counter();
+        showCounter(currentCount);
+    }
+
 }
 
 main();
