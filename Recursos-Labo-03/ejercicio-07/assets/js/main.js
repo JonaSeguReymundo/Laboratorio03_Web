4ee01e20e1488f0c0c0009c7130e67f5178bdfca
JonaSeguReymundo <00043823@uca.edu.sv>
2024-09-03 14:55:01 -0600

    Comentarios del ejercicio 07

--- Recursos-Labo-03/ejercicio-07/assets/js/main.js
+++ Recursos-Labo-03/ejercicio-07/assets/js/main.js
@@ -1,9 +1,22 @@
-const requestValue = (/*recibe*/) => {
-    //Code 
+const requestValue = (message) => {
+    let value;
+    do {
+        value = prompt(message);
+    } while (value === null || value.trim() === ""); // Para que no este vacia
+    return value;
 }
 
 const main = () => {
-    //Code
+    // Pedimos los valores de a y b al usuario
+    let a = requestValue("Introduce el valor de a:");
+
+    let b = requestValue("Introduce el valor de b:");
+
+    // Intercambio
+    [a, b] = [b, a];
+
+    // Mostramos los valores intercambiados al usuario
+    alert(`Después del intercambio: a = ${a}, b = ${b}`);
 }
 
 main();
